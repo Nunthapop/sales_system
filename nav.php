9bed72ed44e1047778fdb7662a59e2bfe92daebc
Arthur <nuntapopsangehot@gmail.com>
2024-02-27 01:26:09 +0700

    first setup

--- nav.php
+++ nav.php
@@ -1,17 +1,11 @@
-<?php ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sales_system</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>Sales_system</h1>
     
-
 </body>
-</html>
-
-<?php ?>
+</html>l
