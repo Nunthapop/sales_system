6f4b5120c60505a727285180ab5b4ce704b1c09b
supxnxt <161368582+supxnxt@users.noreply.github.com>
2024-02-28 23:44:39 +0700

    Create shop.php

---
+++ shop/shop.php
@@ -0,0 +1,65 @@
+<?php include "../header/nav.php";
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+</head>
+
+<body>
+    <h1>Shop</h1>
+    <input type="submit" value="card">
+    <table>
+        <thead>
+            <tr>
+                <th>Product Image</th>
+                <th>Product Name</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td><img src="product1.jpg" alt="Product 1"></td>
+                <td>Product 1</td>
+                <td>Description of Product 1.</td>
+                <td>$19.99</td>
+                <td><button>Add to Cart</button></td>
+            </tr>
+            <tr>
+                <td><img src="product2.jpg" alt="Product 2"></td>
+                <td>Product 2</td>
+                <td>Description of Product 2. </td>
+                <td>$29.99</td>
+                <td><button>Add to Cart</button></td>
+            </tr>
+            <tr>
+                <td><img src="product3.jpg" alt="Product 3"></td>
+                <td>Product 3</td>
+                <td>Description of Product 3. </td>
+                <td>$39.99</td>
+                <td><button>Add to Cart</button></td>
+            </tr>
+            <tr>
+                <td><img src="product4.jpg" alt="Product 4"></td>
+                <td>Product 4</td>
+                <td>Description of Product 4. </td>
+                <td>$49.99</td>
+                <td><button>Add to Cart</button></td>
+            </tr>
+            <tr>
+                <td><img src="product5.jpg" alt="Product 5"></td>
+                <td>Product 5</td>
+                <td>Description of Product 5. </td>
+                <td>$59.99</td>
+                <td><button>Add to Cart</button></td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+
+</html>
