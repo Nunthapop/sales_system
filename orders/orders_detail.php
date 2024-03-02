b0cb179dae6fa62ecad7d8ceb477d104836b2ecf
Arthur <nuntapopsangehot@gmail.com>
2024-03-03 00:34:49 +0700

    update orders_Detail

--- orders/orders_detail.php
+++ orders/orders_detail.php
@@ -21,97 +21,121 @@ $result = mysqli_query($connect, $query); ?>
 $query = "SELECT * FROM orders join orders_detail using (orders_id) 
 join customers using(customers_id) join product using (product_id) join membership using (membership_id) join employee using(employee_id) where orders_id =$id";
 $result2 = mysqli_query($connect, $query); ?>
-          <?php if ($row = mysqli_fetch_assoc($result2)) {
-             
-             ?>
-<body>
-    <main>
-        <div class="parent-box">
-            <div class="header_title">
-               <h2>Orders Detail || P.o.# <?php echo $id; ?> </h2> 
-               <div class="box2"> <h3>Customer ID:<?php  echo $row['customers_id'];?></h3>
-               <h3> Date issued:<?php  echo $row['date'];?></h3>
-               <h3>   Sales person:<?php  echo $row['emp_name'];?></h3>
-               
-             
-            </div>
-                
-            </div>
-  
+<?php if ($row = mysqli_fetch_assoc($result2)) {
 
-            <div class="delivery">
-                <div class="bill">
-                    <h3> Bill to:</h3><?php  
-                    echo $row['cus_first_name'];  
-                    echo $row['cus_last_name'];?>
-                    <h4>  <?php  echo $row['address'];?></h4>
-                    <h4>  <?php  echo $row['telephone'];?></h4>
-               
+?>
+
+    <body>
+        <main>
+            <div class="parent-box">
+                <div class="header_title">
+                    <h2>Orders Detail || P.o.# <?php echo $id; ?> </h2>
+                    <div class="box2">
+                        <h3>Customer ID:<?php echo $row['customers_id'] . ' :' . $row['member_name']; ?></h3>
+                        <h3> Date issued:<?php echo $row['date']; ?></h3>
+                        <h3> Sales person:<?php echo $row['emp_name']; ?></h3>
+                    </div>
+
                 </div>
-                <div class="ship">
-                <h3> Ship to:</h3><?php  
-                    echo $row['cus_first_name'];  
-                    echo $row['cus_last_name'];?>s
-                    <h4>  <?php  echo $row['address'];?></h4>
-                    <h4>  <?php  echo $row['telephone'];?></h4>
-                <?php }?>
+
+
+                <div class="delivery">
+                    <div class="bill">
+                        <h3> Bill to:</h3><?php
+                                            echo $row['cus_first_name'];
+                                            echo $row['cus_last_name']; ?>
+                        <h4> <?php echo $row['address']; ?></h4>
+                        <h4> <?php echo $row['telephone']; ?></h4>
+
+                    </div>
+                    <div class="ship">
+                        <h3> Ship to:</h3><?php
+                                            echo $row['cus_first_name'];
+                                            echo $row['cus_last_name']; ?>s
+                        <h4> <?php echo $row['address']; ?></h4>
+                        <h4> <?php echo $row['telephone']; ?></h4>
+                    <?php } ?>
+                    </div>
+
                 </div>
-              
-            </div>
-            <TABLE>
-                <tr>
-                    <th>ITEMS</th>
-                    <th>DESCRIPTION</th>
-                    <th>PRICE </th>
-                    <th>product_type</th>
-                    <th> QTY</th>
-                    <th> TOTAL</th>
-                </tr>
-                <?php
-                $previousOrderId = null;
-                $total = 0;
-                while ($row = mysqli_fetch_assoc($result)) {
-
-                    //$total =$row['orders_total'];
-                    $name = $row['cus_first_name'] . ' ' . $row['cus_last_name'];
-                    $total += $row['product_price'] * $row['qty'];
-                    $membership_dis = $row['member_discount'];
-                ?>
-
-
-
+                <TABLE>
                     <tr>
-                        <td><?php echo $row['product_name']; ?></td>
-                        <td><?php echo $row['product_description']; ?></td>
-                        <td><?php echo $row['product_price']; ?></td>
-                        <td><?php echo $row['product_type']; ?></td>
-                        <td><?php echo $row['qty']; ?></td>
-                        <td><?php echo $row['product_price'] * $row['qty']; ?></td>
-
+                        <th>ITEMS</th>
+                        <th>DESCRIPTION</th>
+                        <th>PRICE </th>
+                        <th>product_type</th>
+                        <th> QTY</th>
+                        <th> TOTAL</th>
                     </tr>
-                <?php } ?>
-            </TABLE>
+                    <?php
+                    $previousOrderId = null;
+                    $subtotal = 0;
+                    while ($row = mysqli_fetch_assoc($result)) {
+
+                        //$total =$row['orders_total'];
+                        $name = $row['cus_first_name'] . ' ' . $row['cus_last_name'];
+                        $subtotal += $row['product_price'] * $row['qty'];
+                        $membership_dis = $row['member_discount'];
+                    ?>
 
 
-            <?php //echo $total  . $name; 
-            echo $total; ?>
-</body>
+
+                        <tr>
+                            <td><?php echo $row['product_name']; ?></td>
+                            <td><?php echo $row['product_description']; ?></td>
+                            <td><?php echo $row['product_price']; ?></td>
+                            <td><?php echo $row['product_type']; ?></td>
+                            <td><?php echo $row['qty']; ?></td>
+                            <td><?php echo $row['product_price'] * $row['qty']; ?></td>
+
+                        </tr>
+                    <?php } ?>
+                </TABLE>
+                <table>
+                    <tr>
+                        <th>Subtotal</th>
+                        <th>Discount</th>
+                        <th>Tax </th>
+                        <th>Delivery</th>
+                        <th>TOTAL</th>
+                    </tr>
+                    <?php
+                    $discount = 10; //
+                    $dis_price = 0;
+                    $total = 0;
+                    //calculate discount for requirement and membership discount
+                    if ($subtotal >= 20) {
+                        $dis_price = ($subtotal / 100) * ($discount + $membership_dis);
+                        $total = $subtotal - $dis_price;
+                    } else {
+                        $dis_price = ($subtotal / 100) * $membership_dis;
+                        $total = $subtotal - $dis_price;
+                    }
+
+
+
+                    ?>
+                    <tr>
+                        <td><?php
+                            echo $subtotal; ?></td>
+                        <td><?php echo $discount; ?></td>
+                        <td>free</td>
+                        <td>free</td>
+                        <td><?php echo $total; ?></td>
+                    </tr>
+                </table>
+
+                </div>
+                <!-- Box end -->
+                <div class="overall">
+                    <div class="status">
+                        Status: wait for Payment
+                    </div>
+                    <div class="invoices">
+                        <a href="">Print receipt</a>
+                    </div>
+                </div>
+        </main>
+    </body>
 
 </html>
-<?php
-$discount = 10; //
-$dis_price = 0;
-$orders_total = 0;
-//calculate discount for requirement and membership discount
-if ($total >= 20) {
-    $dis_price = ($total / 100) * ($discount + $membership_dis);
-    $orders_total = $total - $dis_price;
-} else {
-    $dis_price = ($total / 100) * $membership_dis;
-    $orders_total = $total - $dis_price;
-}
-echo 'net' . $orders_total;
-
-
-?> </div>
-</main>
