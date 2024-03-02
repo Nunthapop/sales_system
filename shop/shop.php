a570c03c0220c157ef675dcf7e4f498e69c16245
supxnxt <161368582+supxnxt@users.noreply.github.com>
2024-03-03 00:37:33 +0700

    กูจอยได้แล้วไอ่สัส

--- shop/shop.php
+++ shop/shop.php
@@ -9,14 +9,15 @@ include "../header/nav.php";
 <body>
     <div class="main">
         <?php
-        $query = "SELECT * FROM orders";
-        $query = "SELECT * FROM orders_detail";
-        $result = mysqli_query($connect, $query,); ?>
+        $query = "SELECT o.orders_id,o.date,o.customers_id,od.qty,od.product_id FROM orders o inner join orders_detail od on o.orders_id = od.orders_id";
+        $result = mysqli_query($connect, $query, ); ?>
         <table>
             <tr>
                 <th>Order_id</th>
+                <th>Date</th>
                 <th>Customer_id</th>
                 <th>Quantity</th>
+                <th>Product_id</th>
             </tr>
             <?php while ($row = mysqli_fetch_assoc($result)) { ?>
                 <tr>
@@ -24,10 +25,16 @@ include "../header/nav.php";
                         <?php echo $row['orders_id']; ?>
                     </td>
                     <td>
+                        <?php echo $row['date']; ?>
+                    </td>
+                    <td>
                         <?php echo $row['customers_id']; ?>
                     </td>
                     <td>
                         <?php echo $row['qty']; ?>
+                    </td>
+                    <td>
+                        <?php echo $row['product_id']; ?>
                     </td>
                 </tr>
             <?php } ?>
