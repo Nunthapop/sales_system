14be54beb50ab7b19a8d260a263425443f6ebedf
supxnxt <161368582+supxnxt@users.noreply.github.com>
2024-03-03 16:44:28 +0700

    update

--- config/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-session_unset();
-header ('Refresh:0; url=Homepage.php');
-?>
--- header/nav.php
+++ header/nav.php
@@ -11,17 +11,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/nav.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Jost:wght@200;400;800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Jost:wght@200;400;800&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet">
     <title>navigate bar</title>
     <style> </style>
 </head>
 
 <body>
-    
+
     <div class="nav">
         <ul>
-        <li><a href="../dashboard/dashboard.php">Dashboard</a></li>
+            <li><a href="../dashboard/dashboard.php">Dashboard</a></li>
             <li><a href="../Product/product.php"> Product</a></li>
             <li> <a href="../employee/emp.php">Employee</a></li>
             <li> <a href="../customer/customer.php">customer</a></li>
@@ -30,8 +32,7 @@
             <li> <a href="../shop/shop.php">Shop</a></li>
             <li> <a href="../login/logout.php">log out</a></li>
         </ul>
-        <?php echo  $_SESSION['emp_position'];?>
-        
+
     </div>
 
 </body>
