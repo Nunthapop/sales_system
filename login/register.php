0e2612e854bb13e5d0a58293b8a227a07ed4814e
tarnkkk9 <narabodee_klaywong@cmu.ac.th>
2024-03-07 05:02:12 +0700

    Update register.php

--- login/register.php
+++ login/register.php
@@ -1,5 +1,45 @@
 <?php
 include_once "../config/config.php";
 session_start();
-session_destroy();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    
+    $query = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$hashed_password')";
+    
+    if (mysqli_query($connection, $query)) {
+        echo "Registration successful!";
+    } else {
+        echo "Error: " . $query . "<br>" . mysqli_error($connection);
+    }
+    
+    mysqli_close($connection);
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Register</title>
+</head>
+<body>
+    <h2>Register</h2>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required><br><br>
+        
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required><br><br>
+        
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required><br><br>
+        
+        <input type="submit" value="Register">
+    </form>
+</body>
+</html>
