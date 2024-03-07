3c442de2d8ece1057cca0a37b0444f5e6e3d643c
Arthur <nuntapopsangehot@gmail.com>
2024-03-07 23:05:35 +0700

    register

--- login/login_cus.php
+++ login/login_cus.php
@@ -14,24 +14,29 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/login.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Jost:wght@100..900&family=Montserrat+Subrayada:wght@400;700&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <title>Login</title>
 </head>
 
 <body>
     <form action="check_login_cus.php" method="post">
-        <div class="input">
-            <h1>Customer</h1>
-            <label class="username">Email<br></label>
-            <input type="text" id="username" name="email">
-        </div>
-        <div class="input">
-            <label for="">PASSWORD<br></label>
-            <input type="password" id="password" name="password">
+        <div class="login">
+            <div class="input">
+                <h1>Customer</h1>
+                <label class="username">Email<br></label>
+                <input type="text" id="username" name="email">
+            </div>
+            <div class="input">
+                <label for="">PASSWORD<br></label>
+                <input type="password" id="password" name="password">
+
+            </div>
+            <input type="submit" value="LOGIN NOW">
+            <div class="forget"> Not a member yet?<a href="register.php"> Register now</a></div>
 
         </div>
-        <input type="submit" value="LOGIN NOW">
-        <div class="forget"> Not a member yet?<a href="register.php"> Register now</a></div>
-
         </div>
+
     </form>
 </body>
