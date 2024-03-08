00f0ddca76190d8dfdc41264a1f52e691051ed37
supxnxt <161368582+supxnxt@users.noreply.github.com>
2024-03-08 21:35:37 +0700

    up

--- shop/history.php
+++ shop/history.php
@@ -5,6 +5,7 @@ include "../header/nav_cus.php";
 <!DOCTYPE html>
 
 <body>
+
     <h1>History</h1>
     <table>
         <tr>
@@ -14,25 +15,28 @@ include "../header/nav_cus.php";
             <td>Payment</td>
         </tr>
         <?php
+
+
+        ?>
+        <?php
         $query = "SELECT * FROM orders_detail join orders using (orders_id) join invoices using (orders_id) where customers_id =  '" . $_SESSION['customers_id'] . "' ";
         $result = mysqli_query($connect, $query);
-        $row = mysqli_fetch_array($result);
-        ?>
-
-        <tr>
-            <td>
-                <?= $row['orders_id'] ?>
-            </td>
-            <td>
-                <?= $row['date'] ?>
-            </td>
-            <td>
-                <?= $row['orders_total'] ?>
-            </td>
-            <td>
-                <?= "Cash" ?>
-            </td>
-        </tr>
+        while ($row = mysqli_fetch_array($result)) { ?>
+            <tr>
+                <td>
+                    <?= $row['orders_id'] ?>
+                </td>
+                <td>
+                    <?= $row['date'] ?>
+                </td>
+                <td>
+                    <?= $row['orders_total'] ?>
+                </td>
+                <td>
+                    <?= "Cash" ?>
+                </td>
+            </tr>
+        <?php } ?>
     </table>
 </body>
 
