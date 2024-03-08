65c2f34fe9bdec1817c8672c5f8f9ba55d6e84be
supxnxt <161368582+supxnxt@users.noreply.github.com>
2024-03-08 21:18:46 +0700

    Update history.php

--- shop/history.php
+++ shop/history.php
@@ -1,7 +1,6 @@
 <?php
 include "../header/nav_cus.php";
-$query = "SELECT * FROM orders_detail join orders using (orders_id) join invoices using (orders_id) where customers_id = '" . $_SESSiON['customers_id'] . "' ";
-$result = mysqli_query($connect, $query);
+
 ?>
 <!DOCTYPE html>
 
@@ -9,10 +8,30 @@ $result = mysqli_query($connect, $query);
     <h1>History</h1>
     <table>
         <tr>
-            <td>Product_Name</td>
-            <td>Prod</td>
-            <td></td>
-            <td></td>
+            <td>Order id</td>
+            <td>Date</td>
+            <td>Order Total</td>
+            <td>Payment</td>
+        </tr>
+        <?php
+        $query = "SELECT * FROM orders_detail join orders using (orders_id) join invoices using (orders_id) where customers_id =  '" . $_SESSION['customers_id'] . "' ";
+        $result = mysqli_query($connect, $query);
+        $row = mysqli_fetch_array($result);
+        ?>
+
+        <tr>
+            <td>
+                <?= $row['orders_id'] ?>
+            </td>
+            <td>
+                <?= $row['date'] ?>
+            </td>
+            <td>
+                <?= $row['orders_total'] ?>
+            </td>
+            <td>
+                <?= "Cash" ?>
+            </td>
         </tr>
     </table>
 </body>
